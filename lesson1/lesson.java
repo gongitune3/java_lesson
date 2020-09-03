740b23bcf239a951c8218ede3934b2ab173f166a
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-03 16:35:57 +0900

    Repetitive practice

--- lesson1/lesson.java
+++ lesson1/lesson.java
@@ -3,3 +3,7 @@ public class Main {
 		System.out.println("hello, world!");
 	}
 }
+
+// public static void main(String[] args)
+// public static void main(String[] args)
+// public static void main(String[] args)
