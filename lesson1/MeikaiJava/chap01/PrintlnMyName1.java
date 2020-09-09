1ce06aa95b5023ca5e9832119a3b3d5aed3344af
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-09 13:27:27 +0900

    纏流子

---
+++ lesson1/MeikaiJava/chap01/PrintlnMyName1.java
@@ -0,0 +1,6 @@
+
+class PrintlnMyName1 {
+	public static void main(String[] args) {
+		System.out.println("纏\n\n流\n子");
+	}
+}
---
+++ lesson1/MeikaiJava/chap01/WhoAreYou1.java
@@ -0,0 +1,7 @@
+
+class WhoAreYou1 {
+	public static void mainz(String[] args) {
+		System.out.println("こんにちは！");
+		System.out.println("元気ですか？");
+	}
+}
