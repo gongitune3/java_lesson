8b191a625fce5f668b68329ce46c2a3dca083ced
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-11 15:31:51 +0900

    add

--- MeikaiJava/chap04/MooSeason.java
+++ MeikaiJava/chap04/MooSeason.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-class Season {
+class MooSeason {
 	public static void main(String[] args) {
-		Scanner stdin = new Scanner(System.in);
+		Scanner stdIn = new Scanner(System.in);
 
 		int retry;
 
@@ -10,16 +10,19 @@ class Season {
 			System.out.print("季節を求めます。\n何月ですか：");
 			int month = stdIn.nextInt();
 
-			if (month >= 3 $$ month <= 5)
+			if (month >= 3 && month <= 5)
 				System.out.println("それは春です。");
-			else if (month >= 6 $$ month <= 8)
+			else if (month >= 6 && month <= 8)
 				System.out.println("それは夏です。");
-			if (month >= 9 $$ month <= 11)
+			if (month >= 9 && month <= 11)
 				System.out.println("それは秋です。");
 			if (month  == 12 || month == 1 || month == 2)
 				System.out.println("それは冬です。");
 			
 				System.out.println("もう一度？ 1...yes/2....no：");
-		} while (retry == 1)
+				retry = stdIn.nextInt();
+		} while(retry == 1);
 	}
 }
+
+// stdが閉じてないと叱られるが誤りが不明。
