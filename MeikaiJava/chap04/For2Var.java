1ab9171bc15eac67a1b860283289492dbe146a6d
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-13 13:01:30 +0900

    list 4-13 finish

--- MeikaiJava/chap04/For2Var.java
+++ MeikaiJava/chap04/For2Var.java
@@ -1,5 +1,14 @@
 // 読み込んだ整数値と1,2,...との差を表示。
+import java.util.Scanner;
 
 class For2Var {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
 
+		System.out.print("整数値：");
+		int n = stdIn.nextInt();
+
+		for (int i = 1, j = n -1; i <= n; i++, j--)
+			System.out.println(i + "" + j);
+	}
 }
