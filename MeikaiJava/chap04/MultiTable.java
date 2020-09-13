59111d595ca52b2a704a2200a5e5020531e335c5
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-13 14:28:44 +0900

    finish

--- MeikaiJava/chap04/MultiTable.java
+++ MeikaiJava/chap04/MultiTable.java
@@ -5,9 +5,9 @@ class MultiTable {
 		for (int i = 1; i <= 9; i++){
 			for(int j = 1; j <= 9; j++) {
 				if (i * j < 10)
-				System.out.print("  ");
+					System.out.print("  ");
 				else 
-				System.out.print(" ");
+					System.out.print(" ");
 				System.out.print(i * j);
 			}
 		}
