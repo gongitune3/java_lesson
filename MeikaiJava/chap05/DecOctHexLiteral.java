7a4d18c507985907126e798f57dd109106bf10f1
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-14 14:29:43 +0900

    list 5-1 finish

--- MeikaiJava/chap05/DecOctHexLiteral.java
+++ MeikaiJava/chap05/DecOctHexLiteral.java
@@ -1,5 +1,13 @@
 //整数リテラル（10進数/8進数/16進数）
 
 class DecOctHexLiteral {
+	public static void main(String[] args) {
+		int a = 13;		//10進数
+		int b = 013;	//8進数
+		int c = 0x13;	//16進数
 
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+	}
 }
