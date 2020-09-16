ffe20fc99c9763c300f1497f57b9d6487f36dc66
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-16 15:08:03 +0900

    list 5-10

--- MeikaiJava/chap05/IntergralToFloat.java
+++ MeikaiJava/chap05/IntergralToFloat.java
@@ -4,7 +4,12 @@ class IntergralToFloat {
 	public static void main(String[] args) {
 		
 		int a = 123456789;
-		long b = 
-	}
+		long b = 1234567890123456789L;
 
+		System.out.println(" 		a = " + 		a);
+		System.out.println("(float) a = " +  (float)a);
+		
+		System.out.println(" 		b = " + 		b);
+		System.out.println("(double)b = " + (double)b);
+	}
 }
