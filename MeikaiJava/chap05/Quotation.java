4a8492c40837e1a6c91fe5b3572a0a9904c3d6c0
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-16 15:56:52 +0900

    \

---
+++ MeikaiJava/chap05/Quotation.java
@@ -0,0 +1,11 @@
+//拡張表記 \"と\'の利用例
+
+class Quotation {
+	public static void main(String[] args) {
+		System.out.println("二重付で囲まれた\"ABC\"は文字リテラルです。");
+
+		System.out.print("一重符で囲まれた");
+		System.out.print('\'');
+		System.out.println("A'は文字リテラルです。");
+	}
+}
