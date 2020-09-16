49dec093fda8513807927fad9a30b395e4edb57c
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-16 15:41:00 +0900

    list 5-11 finish

--- MeikaiJava/chap05/CarriageReturn.java
+++ MeikaiJava/chap05/CarriageReturn.java
@@ -1,4 +1,8 @@
+//復帰の出力によって表示済み文字を切り替える
 
 class CarriageReturn {
-
+	public static void main(String[] args) {
+		System.out.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		System.out.println("\r12345");
+	}
 }
