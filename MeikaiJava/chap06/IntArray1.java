a731556ebc67561ecb0f561783cb094f60d8a1fa
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-17 13:49:44 +0900

    list 6-2 finish

--- MeikaiJava/chap06/IntArray1.java
+++ MeikaiJava/chap06/IntArray1.java
@@ -2,8 +2,28 @@
 
 class IntArray1 {
 	public static void main(String[] args) {
-		int[] a = new.int[5];		//配列の宣言
+		int[] a = new int[5];		//配列の宣言
 
+		a[1] = 37;
+		a[2] = 51;
+		a[4] = a[1] * 2;
 
+		//全体の値を表示
+
+		System.out.println("a[0] =" + a[0]);
+		System.out.println("a[1] =" + a[1]);
+		System.out.println("a[2] =" + a[2]);
+		System.out.println("a[3] =" + a[3]);
+		System.out.println("a[4] =" + a[4]);
+		
+
+		//上下とも出力結果は同じだが、何故本は下の記述をしているのか
+		//わかりやすさのため？？
+
+		System.out.println("a[" + 0 + "] = " + a[0]);
+		System.out.println("a[" + 1 + "] = " + a[1]);
+		System.out.println("a[" + 2 + "] = " + a[2]);
+		System.out.println("a[" + 3 + "] = " + a[3]);
+		System.out.println("a[" + 4 + "] = " + a[4]);
 	}
 }
