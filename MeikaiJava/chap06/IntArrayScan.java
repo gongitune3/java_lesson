3594172c708db0754765bc85cb7e9a6a0dffd79a
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-17 14:32:55 +0900

    list 6-4 start

---
+++ MeikaiJava/chap06/IntArrayScan.java
@@ -0,0 +1,14 @@
+//配列の全要素に値を読み込んで表示
+
+import java.util.Arrays
+import java.util.Scanner
+
+class IntArrayScan {
+	public static void main(String[] args) {
+		Scanner.out.print("要素数：");
+		int n = stdIn.nextInt();	//要素数を読み込む
+		int[] a = new int[n];		//配列を生成
+
+
+	}
+}
