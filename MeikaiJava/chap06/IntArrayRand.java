225e9429219544c7139136194a9104bafafea1b2
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-17 15:09:40 +0900

    list 6-5 finish

--- MeikaiJava/chap06/IntArrayRand.java
+++ MeikaiJava/chap06/IntArrayRand.java
@@ -9,11 +9,18 @@ class IntArrayRand {
 		Random rand = new Random();
 		Scanner stdIn = new Scanner(System.in);
 
-		System.out,print("要素数：");
+		System.out.print("要素数：");
 		int n = stdIn.nextInt();
 		int[] a = new int[n];
 
 		for (int i = 0; i < n; i++)
-			a[i] = 1+ rand.nextint(10);		//1〜10の乱数
+			a[i] = 1 + rand.nextInt(10);		//1〜10の乱数
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("a[" + i + "] : ");
+			for(int j = 0; j < a[i]; j++)
+				System.out.print("*");
+			System.out.println();
+		}
 	}
 }
