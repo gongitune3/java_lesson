b76968ac7d4e1f25a963ac211b16b8920998c6d4
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-17 14:27:51 +0900

    add

--- MeikaiJava/chap06/IntArray2.java
+++ MeikaiJava/chap06/IntArray2.java
@@ -4,10 +4,14 @@ class IntArray2 {
 	public static void main(String[] args) {
 		int[] a = new int [5];	//配列の宣言
 
+		for (int i = 0; i < a.length; i++) 
+		a[i] = i + 1;
+		
 		for (int i = 0; i < a.length; i++)
-			a[i] = i + 1;
-
-		for (int i = 0; i < a.length; i++)
-			System.out.println("a["+ i +"] = " + a[i] );
+		System.out.println("a["+ i +"] = " + a[i] );
 	}
 }
+
+//配列変数名.lengthで要素数＝長さを取得できる
+//ここでは５が取れる
+//lengthはint型でなく、fainal　intでございます。その為、tame,lengthに値を導入することはできません。
