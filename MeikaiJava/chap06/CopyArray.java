b971eb4bd5cd3f235b808d58dd936f2b01a19ca6
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-19 11:39:35 +0900

    list6-10*

---
+++ MeikaiJava/chap06/CopyArray.java
@@ -0,0 +1,22 @@
+//list 6-10*
+
+import java.util.Arrays;
+
+class CopyArray {
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5};
+		int[] b = {6,5,4,3,2,1,0};
+
+		System.out.println("a = " + Arrays.toString(a));
+		System.out.println("b = " + Arrays.toString(b));
+
+		if (a.length != b.length)				//要素の数を揃えている
+			b = new int[a.length];
+
+		for (int i = 0; i < a.length; i++)		//配列aの全要素を配列bにコピー
+			b[i] = a[i];
+
+		System.out.println("a = " + Arrays.toString(a));
+		System.out.println("b = " + Arrays.toString(b));
+	}
+}
