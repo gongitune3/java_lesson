e988d80a2ca1a1209b1dc6a78a28efef77ebc7f9
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-19 15:34:25 +0900

    list 7-6 finish

--- MeikaiJava/chap07/Power1.java
+++ MeikaiJava/chap07/Power1.java
@@ -1,6 +1,8 @@
 //べき乗を求める
 //list 7-6
 
+import java.util.Scanner;
+
 class Power1 {
 	static double power(double x, int n) {
 		double tmp = 1.0;
@@ -12,9 +14,16 @@ class Power1 {
 		// ↑でも可能
 		return tmp;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("aのb乗を求めます。");
+		System.out.print("実数a：");	double a = stdIn.nextDouble();
+		System.out.print("整数b：");	int b = stdIn.nextInt();
+
+		System.out.println(a + "の" + b + "乗は" + power(a, b) + "です。" );
+
+
 	}
 }
