cb78441fb09a64c73c7f77c316633976fe855faf
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-19 10:32:59 +0900

    list 6-8 finish

--- MeikaiJava/chap06/HighScore.java
+++ MeikaiJava/chap06/HighScore.java
@@ -6,13 +6,19 @@ import java.util.Scanner;
 class HighScore {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
-		fainal int NUMBER = 5;
-		int[] tensu = new int[NUMBER];
+		final int NUMBER = 5;				//人数
+		int[] tensu = new int[NUMBER];		//点数
 
 		System.out.println(NUMBER + "人の点数を入力せよ。");
-		for (int i = 0; i < NUMBER; i++)
+		for (int i = 0; i < NUMBER; i++){
 			System.out.print((i + 1) + "番の点数：");
 			tensu[i] = stdIn.nextInt();
-			
+		}
+
+		int  max = tensu[0];
+		for (int i = 1; i < tensu.length; i++)
+			if (tensu[i] > max) max = tensu[i];
+
+		System.out.println("最高点は" + max + " 点です。");
 	}
 }
