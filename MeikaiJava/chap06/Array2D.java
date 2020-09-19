9da61409d27aab036bbba6e5e9b4ae8f554f96f2
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-19 13:37:43 +0900

    list 6-13 start

---
+++ MeikaiJava/chap06/Array2D.java
@@ -0,0 +1,19 @@
+//２次元配列を生成して、全要素を乱数で埋め尽くす
+//list 6-13
+
+import java.util.Scanner;
+import java.util.Random;
+
+class Array2D {
+public static void main(String[] args) {
+	Random rand = new Random();
+	Scanner stdIn = new Scanner(System.in);
+
+	System.out.print("行数：");
+	int h = stdIn.nextInt();		//行を読み込む
+
+	System.out.print("列数：");
+	int w = stdIn.nextInt();
+
+	}
+}
