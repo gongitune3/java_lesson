af707f2a86456c8cd803ccb4e1c4c574a428234c
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-19 11:26:03 +0900

    list 6-10 finish

--- MeikaiJava/chap06/AssingArray.java
+++ MeikaiJava/chap06/AssingArray.java
@@ -1,6 +1,21 @@
 //配列の代入(誤り)
 //list 6-10
 
-class AssingArray {
+import java.util.Arrays;
 
+class AssingArray {
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5};
+		int[] b = {6,5,4,3,2,1,0};
+
+		System.out.println("a = " + Arrays.toString(a));
+		System.out.println("b = " + Arrays.toString(b));
+
+		b = a; 		//配列のコピー？？？
+		a[0] = 10;	//配列a[0]の値を書き換える
+
+		System.out.println("aをbに代入してa[0]に10を入れました。");
+		System.out.println("a = " + Arrays.toString(a));
+		System.out.println("b = " + Arrays.toString(b));
+	}
 }
