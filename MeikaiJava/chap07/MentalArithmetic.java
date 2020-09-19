5401af3525e115257c7debc18bed9eb1ba3ecab1
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-19 17:32:54 +0900

    add

--- MeikaiJava/chap07/MentalArithmetic.java
+++ MeikaiJava/chap07/MentalArithmetic.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 
 
 class MentalArithmetic {
-	
+	static Scanner stdIn = new Scanner(System.in);
+
+	//--続行の確認--//
+	static boolean confirmRetry {
+		int count;
+		do {
+			System.out.print("もう一度?<Yes…1/No…0>：");
+			count = stdIn.nextInt();
+		}	while (count != 0 && count != 1);
+		return count == 1;		//countが1であればtrun、そうでなければfalseを返す。
+	}
 
 }
