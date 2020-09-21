c46829dfb7767d69168b9d5fdb7c57b686005eeb
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-21 13:13:15 +0900

    list 7-18 finish

--- MeikaiJava/chap07/ArrayClone.java
+++ MeikaiJava/chap07/ArrayClone.java
@@ -1,7 +1,33 @@
 //配列の複製を作成
 //list 7-18
 
+import java.util.Scanner;
 
 class ArrayClone {
+	//--配列aの複製を作成して返却--//
+	static int[] cloneOf(int[] a) {
+		int[] c = new int[a.length];
+		for (int i = 0; i < a.length; i++)
+			c[i] = a[i];
+		return c;
+	}
 
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("要素数：");
+		int num = stdIn.nextInt();
+		int[] x = new int[num];
+
+		for (int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "]：");
+			x[i] = stdIn.nextInt();
+		}
+
+		int[] y = cloneOf(x);
+
+		System.out.println("配列xの複製yを作成しました。");
+		for (int i = 0; i < num; i++)
+			System.out.println("y[" + i + "] = " + y[i]);
+	}
 }
