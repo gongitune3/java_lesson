4c22977f4fcde97fb30bc1046d2b28030b799746
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-22 12:59:00 +0900

    list 8-4 start

---
+++ MeikaiJava/car1/Car.java
@@ -0,0 +1,17 @@
+//自動車クラス[第1版]
+//list 8-4
+
+class Car {
+	private String name;
+	private int width;
+	private int height;
+	private int length;
+	private double x;
+	private double y;
+	private double fuel;
+
+	Car(String name, int width, int height, int length, double fuel) {
+		this.name = name;
+		this.length = length;	this.fuel
+	}
+}
