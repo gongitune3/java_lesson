47d0ed09a40ed356d5e0fb5ed526239ea9a09a4a
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-29 10:45:58 +0900

    add

--- MeikaiJava/chap09/Day.java
+++ MeikaiJava/chap09/Day.java
@@ -2,8 +2,13 @@
 //list 9-1
 
 class Day {
-	private int = year;
-	private int = month;
-	private int = date;
+	private int year;
+	private int month;
+	private int date;
 }
 
+Day(int year, int month, int date) {
+	this.year 	= year;
+	this.month 	= month;
+	this.date 	= date;
+}
