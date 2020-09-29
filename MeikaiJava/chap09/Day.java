75195a11b81b14b9800105f4dbfd96afbce32697
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-29 11:09:28 +0900

    list 9-1 finish

--- MeikaiJava/chap09/Day.java
+++ MeikaiJava/chap09/Day.java
@@ -5,26 +5,37 @@ class Day {
 	private int year;
 	private int month;
 	private int date;
-}
-
-Day(int year, int month, int date) {
-	this.year 	= year;
-	this.month 	= month;
-	this.date 	= date;
-}
-
-//--年・月・日を取得
-int getYear(int year) {this.year = year;}
-int getMonth(int month) {this.month = month;}
-int getDate(int date) {this.date = date;}
-
-//--年・月・日を設定
-void setYear(int year) {this.year = year;}
-void setMonth(int month) {this.month = month;}
-void setDate(int date) {this.date = date;}
-
-void set(int year, int month, int date) {
-	this.year = year;
-	this.month = month;
-	this.date = date;
+	
+	Day(int year, int month, int date) {
+		this.year 	= year;
+		this.month 	= month;
+		this.date 	= date;
+	}
+	
+	//--年・月・日を取得
+	int getYear() {return year;}
+	int getMonth() {return month;}
+	int getDate() {return date;}
+	
+	//--年・月・日を設定
+	void setYear(int year) {this.year = year;}
+	void setMonth(int month) {this.month = month;}
+	void setDate(int date) {this.date = date;}
+	
+	void set(int year, int month, int date) {
+		this.year = year;
+		this.month = month;
+		this.date = date;
+	}
+	
+	//曜日を決める
+	int OfWeek() {
+		int y = year;
+		int m = month;
+		if (m == 1 || m == 2) {
+			y--;
+			m += 12;
+		}
+		return (y + y / 4 - y / 100 + y / 400 + (13 * m + 8) / 5 + date ) % 7;
+	}
 }
