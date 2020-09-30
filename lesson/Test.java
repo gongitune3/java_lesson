2d54f09d80b5e57e81454ecb252e96c301d1d318
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-30 10:11:15 +0900

    test add

--- lesson/Test.java
+++ lesson/Test.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class Test {
 	public static void main(String[] args) {
-		
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("テストを始めますが宜しいでしょうか？？");
 	}
 }
