3b5271cc5d25d8227af294c883129e2a6193270f
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-30 10:24:41 +0900

    test add

--- lesson/Test.java
+++ lesson/Test.java
@@ -4,6 +4,9 @@ class Test {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 
-		System.out.print("テストを始めますが宜しいでしょうか？？");
+		System.out.println("テストを始めます");
+		System.out.println("YES…1/NO…0");
+		
+		int x = stdIn.nextInt();
 	}
 }
