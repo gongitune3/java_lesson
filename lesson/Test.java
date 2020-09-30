8635c8bfcb08a9c3c05e641609b8fb9bd63d1410
gongitune3 <rtdmwrtdmw@gmail.com>
2020-09-30 10:28:06 +0900

    fix

--- lesson/Test.java
+++ lesson/Test.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
-class Test {
+class Test123 {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 
 		System.out.println("テストを始めます");
 		System.out.println("YES…1/NO…0");
-		
+
 		int x = stdIn.nextInt();
+
+		if (x == 1)
+			System.out.println("テストを開始します。")
+		else
+			System.out.println("………中止します。")
 	}
 }
