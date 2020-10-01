9761be60fbbbe45056b7e90ff15c240d838712f1
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-01 15:33:46 +0900

    add

--- lesson/Ddd.java
+++ lesson/Ddd.java
@@ -5,3 +5,35 @@ class Ddd {
 		
 	}
 }
+
+
+import java.util.Scanner;
+
+class ArrayClone {
+	//--配列aの複製を作成して返却--//
+	static int[] cloneOf(int[] a) {
+		int[] c = new int[a.length];
+		for (int i = 0; i < a.length; i++)
+			c[i] = a[i];
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("要素数：");
+		int num = stdIn.nextInt();
+		int[] x = new int[num];
+
+		for (int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "]：");
+			x[i] = stdIn.nextInt();
+		}
+
+		int[] y = cloneOf(x);
+
+		System.out.println("配列xの複製yを作成しました。");
+		for (int i = 0; i < num; i++)
+			System.out.println("y[" + i + "] = " + y[i]);
+	}
+}
