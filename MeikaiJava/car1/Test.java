daea3328b72ea8e4a72a6afd4e23c6561ebb5413
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-05 11:41:20 +0900

    add

--- MeikaiJava/car1/Test.java
+++ MeikaiJava/car1/Test.java
@@ -1,7 +1,12 @@
 //上の階層でjava拡張子が動かないので、、、
 
+import java.util.Scanner;
+
 class Test {
 	public static void main(String[] args) {
+		
+
 		System.out.print("テスト");
+		System.out.print("数値の入力："); int n = stdIn.nextInt();
 	}
 }
