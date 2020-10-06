e240703ca71b582fdd7d68a6ed3d38b7f13bf255
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-06 19:32:16 +0900

    add

--- MeikaiJava/car1/Test.java
+++ MeikaiJava/car1/Test.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 class Test {
 	public static void main(String[] args) {
-		
+		Scanner stdIn = new Scanner(System.in);
 
 		System.out.print("テスト");
 		System.out.print("数値の入力："); int n = stdIn.nextInt();
+		int y = n * 2;
+
+		System.out.println("入力した数値を２倍すると…" + y );
 	}
 }
