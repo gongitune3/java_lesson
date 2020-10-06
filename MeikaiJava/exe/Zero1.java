2222a29e5a69b736dd2103a442778861ed9065a7
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-06 20:11:50 +0900

    add

--- MeikaiJava/exe/Zero1.java
+++ MeikaiJava/exe/Zero1.java
@@ -1,4 +1,14 @@
+//list 3-4
+
+import java.util.Scanner;
 
 class Zero1 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
 
+		System.out.print("整数値：");
+		int n = stdIn.nextInt();
+
+		
+	}
 }
