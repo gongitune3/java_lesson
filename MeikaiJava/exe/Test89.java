2301e3bd562e2adef4ed94ed922e43e68f46cf51
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-08 19:28:21 +0900

    add

--- MeikaiJava/exe/Test89.java
+++ MeikaiJava/exe/Test89.java
@@ -1,9 +1,14 @@
 //テスト
 
+import java.util.Scanner;
+
 class Test89 {
 	public static void main(String[] args) {
 		int[] a = {1,2,3,4,5};
 		int[] b = {6,5,4,3,2,1,0};
+
+		System.out.println("a = " + Arrays.toString(a));
+		System.out.println("b = " + Arrays.toString(b));
 
 		
 	}
