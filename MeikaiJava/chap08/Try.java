94dd03f5cfdcebe8fb737f19e651309ed94735b9
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-10 21:06:56 +0900

    add

--- MeikaiJava/chap08/Try.java
+++ MeikaiJava/chap08/Try.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 class Try {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("数字を");
+		int num = stdIn.nextInt();
+
+		System.out.println();
 	}
 }
